27eee32f46e6aefb0324d6c2404b09efff4a766f
Lucas Alves <lucas.developing@gmail.com>
2023-04-11 23:43:16 -0300

    Updates.

--- lib/android/src/main/java/com/rnbluetoothle/RNBluetoothLeModule.java
+++ lib/android/src/main/java/com/rnbluetoothle/RNBluetoothLeModule.java
@@ -1,127 +1,99 @@
 package com.rnbluetoothle;
 
-import androidx.annotation.NonNull;
-
-import com.facebook.react.modules.core.DeviceEventManagerModule;
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.Promise;
-import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.Arguments;
-
-import java.util.Map;
-import java.util.HashMap;
-
 import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
-import android.content.IntentFilter;
-import android.content.Intent;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.Log;
-
 import androidx.annotation.Nullable;
-
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.rnbluetoothle.NativeReactNativeBluetoothLeSpec;
 import com.rnbluetoothle.bluetooth.BluetoothState;
-import com.rnbluetoothle.receivers.BluetoothStateReceiver;
 import com.rnbluetoothle.receivers.GlobalReceiver;
 
 public class RNBluetoothLeModule extends NativeReactNativeBluetoothLeSpec {
 
-    public static String NAME = "ReactNativeBluetoothLe";
-    private ReactApplicationContext reactContext;
-    private BluetoothStateReceiver bluetoothStateReceiver;
-    /**
-     * Receiver that listen to Bluetooth core events in this module.
-     * This receiver only listen intents when the host is on responsive state and the UI is visible.
-     */
-    private GlobalReceiver globalReceiver;
+  public static String NAME = "ReactNativeBluetoothLe";
+  private ReactApplicationContext reactContext;
 
-    RNBluetoothLeModule(ReactApplicationContext context) {
-        super(context);
-        this.reactContext = context;
+  /**
+   * Receiver that listen to Bluetooth core events in this module.
+   * This receiver only listen intents when the host is on responsive state and the UI is visible.
+   */
+  private GlobalReceiver globalReceiver;
+
+  RNBluetoothLeModule(ReactApplicationContext context) {
+    super(context);
+    this.reactContext = context;
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+  /**
+   * Gets whether bluetooth is supported.
+   *
+   * @return
+   */
+  @Override
+  public boolean getIsSupported() {
+    return true;
+  }
+
+  /**
+   * Gets whether bluetooth is supported.
+   *
+   * @return
+   */
+  @Override
+  public void turnOnIfPossible() {}
+
+  /**
+   * Gets whether bluetooth is supported.
+   *
+   * @return
+   */
+  @Override
+  public void turnOffIfPossible() {}
+
+  /**
+   * Starts broadcast receivers.
+   */
+  private void registerGlobalBroadcast() {
+    // Register global listener.
+    if (globalReceiver == null) {
+      globalReceiver = new GlobalReceiver(this.reactContext);
+      reactContext.registerReceiver(
+        this.globalReceiver,
+        this.globalReceiver.createIntentFilter()
+      );
+      Log.v("Bluetooth", "\"GlobalReceiver\" registered receiver.");
     }
+  }
 
-    @Override
-    @NonNull
-    public String getName() {
-        return NAME;
+  /**
+   * Stops broadcast receiver.
+   */
+  private void unregisterGlobalBroadcast() {
+    if (this.bluetoothStateReceiver != null) {
+      this.reactContext.unregisterReceiver(this.bluetoothStateReceiver);
+      this.bluetoothStateReceiver = null;
+      Log.v("Bluetooth", "\"bluetoothStateReceiver\" unregistered receiver.");
     }
+  }
 
-    /**
-     * Gets whether bluetooth is supported.
-     *
-     * @return
-     */
-    @Override
-    public boolean getIsSupported() {
-        return true;
-    }
+  /**
+   * Add a JS module event listener.
+   */
+  @ReactMethod
+  public void addListener(String eventName) {}
 
-    /**
-     * Gets whether bluetooth is supported.
-     *
-     * @return
-     */
-    @Override
-    public void turnOnIfPossible() {
-
-    }
-
-    /**
-     * Gets whether bluetooth is supported.
-     *
-     * @return
-     */
-    @Override
-    public void turnOffIfPossible() {
-
-    }
-
-    /**
-     * Emit event to JS.
-     *
-     * @param eventName
-     * @param args
-     */
-    public void emitReactContextEvent(String eventName, @Nullable WritableMap args) {
-        this.reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                .emit(eventName, args);
-    }
-
-    /**
-     * Start broadcast receiver related to bluetooth state changing.
-     */
-    @Override
-    public void enableStateChange() {
-        this.bluetoothStateReceiver = new BluetoothStateReceiver();
-
-        // Bluetooth state listener receiver.
-        this.reactContext.registerReceiver(this.bluetoothStateReceiver, this.bluetoothStateReceiver.createIntentFilter());
-        Log.v("Bluetooth", "\"BluetoothStateReceiver\" receiver registered.");
-
-        // Register global listener.
-        if (this.globalReceiver == null) {
-            this.globalReceiver = new GlobalReceiver(this.reactContext);
-        }
-        this.reactContext.registerReceiver(this.globalReceiver, this.globalReceiver.createIntentFilter());
-        Log.v("Bluetooth", "\"GlobalReceiver\" registered receiver.");
-    }
-
-    /**
-     * Stops broadcast receiver related to bluetooth state changing.
-     */
-    @Override
-    public void disableStateChange() {
-        if (this.bluetoothStateReceiver != null) {
-
-            this.reactContext.unregisterReceiver(this.bluetoothStateReceiver);
-            this.bluetoothStateReceiver = null;
-            Log.v("Bluetooth", "\"bluetoothStateReceiver\" unregistered receiver.");
-        }
-    }
+  @ReactMethod
+  public void removeListeners(Integer count) {}
 }
--- lib/android/src/main/java/com/rnbluetoothle/bluetooth/receivers/GlobalReceiver.java
+++ lib/android/src/main/java/com/rnbluetoothle/bluetooth/receivers/GlobalReceiver.java
@@ -1,53 +1,152 @@
-package com.rnbluetoothle.receivers;
+package com.rnbluetoothle.bluetooth.receivers;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
-import android.content.IntentFilter;
-import android.content.Intent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.Log;
-
 import androidx.annotation.Nullable;
-
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.Arguments;
-import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
-
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import java.util.HashMap;
 
 /**
  * Global receiver responsible to receive this library broadcast messages and emit to ReactNativeContext.
  */
 public class GlobalReceiver extends BroadcastReceiver {
-    private Runnable callback;
 
-    public GlobalReceiver(Runnable callback) {
-        this.callback = callback;
+  private ReactApplicationContext reactContext;
+  private HashMap<String, String> enabledEvents = new HashMap<String, String>();
+
+  public GlobalReceiver(ReactApplicationContext reactContext) {
+    super();
+    reactContext = reactContext;
+  }
+
+  /**
+   * Sends event back to coupled JS Module.
+   */
+  private void sendJsModuleEvent(String event, WritableMap map) {
+    reactContext
+      .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+      .emit(event, map);
+  }
+
+  /**
+   * Create WritableMap accordingly to "ACTION_STATE_CHANGED" intent.
+   */
+  @Nullable
+  private WritableMap createNativeMapForBluetoothStateChangeIntent(
+    Intent intent
+  ) {
+    WritableMap payload = Arguments.createMap();
+
+    // Current Bluetooth Adapter state.
+    int currentBluetoothState = intent.getIntExtra(
+      BluetoothAdapter.EXTRA_STATE
+    );
+    if (currentBluetoothState == BluetoothAdapter.STATE_OFF) {
+      payload.putString("status", "off");
+    } else if (currentBluetoothState == BluetoothAdapter.STATE_ON) {
+      payload.putString("status", "on");
     }
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        Log.v("Bluetooth", "[GlobalReceiver] Received a intent.");
-        if (intent.getAction().equals(BluetoothStateReceiver.ACTION_STATE_CHANGE)) {
-            String status = intent.getStringExtra("status");
-            Log.v("Bluetooth", "Bluetooth status just changed to: " + status);
-
-            WritableMap payload = Arguments.createMap();
-            payload.putString("status", status);
-
-            Log.v("Bluetooth", context.getClass().getName());
-            this.callback.run("onStageChange", payload);
-        }
+    // Previous Bluetooth Adapter state.
+    int prevBluetoothState = intent.getIntExtra(
+      BluetoothAdapter.EXTRA_PREVIOUS_STATE
+    );
+    if (prevBluetoothState == BluetoothAdapter.STATE_OFF) {
+      payload.putString("prev_status", "off");
+    } else if (prevBluetoothState == BluetoothAdapter.STATE_ON) {
+      payload.putString("prev_status", "on");
     }
 
-    /**
-     * Create a IntentFilter with corresponding receiver supported actions.
-     *
-     * @return
-     */
-    public static IntentFilter createIntentFilter() {
-        IntentFilter createdIntentFilter = new IntentFilter();
-        createdIntentFilter.addAction(BluetoothStateReceiver.ACTION_STATE_CHANGE);
-        return createdIntentFilter;
+    return null;
+  }
+
+  /**
+   * Create a IntentFilter with corresponding receiver supported actions.
+   *
+   * @return
+   */
+  public static IntentFilter createIntentFilter() {
+    IntentFilter filter = new IntentFilter();
+    filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+    return filter;
+  }
+
+  /**
+   * Gets the event name with correct namespace.
+   */
+  public static String createEventName(String event) {
+    return "rnbluetoothle." + event;
+  }
+
+  /**
+   * Enable events.
+   */
+  public void enableEvent(String event) {
+    enabledEvents.put(createEventName(event), event);
+  }
+
+  /**
+   * Disable event.
+   */
+  public void disableEvent(String event) {
+    enabledEvents.remove(createEventName(event));
+  }
+
+  /**
+   * Gets events count.
+   */
+  public int getEventsCount() {
+    return enabledEvents.size();
+  }
+
+  /**
+   * Register this broadcast receiver.
+   */
+  public void register() {
+    reactContext.registerReceiver(this, this.createIntentFilter());
+    Log.v("Bluetooth", "\"GlobalReceiver\" registered.");
+  }
+
+  /**
+   * Unregister this broadcast receiver.
+   */
+  public void unregister() {
+    reactContext.unregisterReceiver(this);
+  }
+
+  /**
+   * On receive a Intent.
+   */
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    if (enabledEvents.size() > 0) {
+      final String className = context.getClass().getName();
+      String action = intent.getAction();
+      Log.v("Bluetooth", this.name + " received a intent: " + action);
+
+      switch (action) {
+        case BluetoothAdapter.ACTION_STATE_CHANGED: // Bluetooth state just changed to On or Off.
+          String eventName = createEventName("onStateChange");
+          if (enabledEvents.containsKey(eventName)) {
+            this.sendJsModuleEvent(
+                eventName,
+                createNativeMapForBluetoothStateChangeIntent(intent)
+              );
+          }
+          break;
+        default:
+          Log.v("Bluetooth", this.name + " received a intent: " + action);
+          break;
+      }
     }
+  }
 }
