a9a101e0811c1e3252f4eae226329511392f1e73
Matheus-Carnauba <matheus.carnauba@sempreceub.com>
2023-05-23 17:12:30 -0300

    teste de conteúdo md
    teste de conteúdo md

--- docs/src/pages/_app.jsx
+++ docs/src/pages/_app.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import "prismjs/themes/prism-tomorrow.css";
+import { MDXProvider } from "@mdx-js/react";
 import '../styles/global.scss';
 import NavBarItems from "@/components/NavBarItems/NavBarItems";
 import SearchAppBar from "@/components/AppBar/AppBar";
 import Blog from "@/components/Content/Blog";
 import StickyFooter from "@/components/StickyFooter/StickyFooter";
 
+const components = {
+  h1: Blog
+}
+
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <MDXProvider components={components}>
       <NavBarItems>
         <SearchAppBar/>
       </NavBarItems>
@@ -16,6 +22,6 @@ export default function App({ Component, pageProps }) {
       {/*<Blog />*/}
 
       <StickyFooter />
-    </>
+      </MDXProvider>
   );
 }
