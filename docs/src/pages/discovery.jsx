5d9c4ae0cba96ff29875111fa9bd6eeb4423874c
lucas bandeira <lucas4workl8tr@gmail.com>
2023-06-06 14:54:17 -0300

    Discovery page created

---
+++ docs/src/pages/discovery.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Discoveries from "../content/Discoveries.mdx"
+import { Box, Toolbar} from "@mui/material";
+
+export default function Discovery() {
+  return (
+    <div className="ml-60 prose prose-2xl">
+      <div className="flex ml-24 pr-64">
+        <div className="flex-row ">
+          <Box
+            component="main"
+            sx={{ flexGrow: 1, p: 3, position:"center" }}
+          >
+            <Toolbar />
+            <Discoveries/>
+          </Box>
+        </div>
+      </div>
+    </div>
+  );
+}
