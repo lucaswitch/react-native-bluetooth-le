e1565ccab2e32c906bda6ba9169ef3d95a583410
lucas bandeira <lucas4workl8tr@gmail.com>
2023-06-06 14:30:39 -0300

    ajuste de css no getting started

--- docs/src/pages/gettingStarted.jsx
+++ docs/src/pages/gettingStarted.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import GS from "../content/GS.mdx"
-import { Box, Toolbar, Typography } from "@mui/material";
-
-const drawerWidth = 240;
+import { Box, Toolbar} from "@mui/material";
 
 export default function GettingStarted() {
   return (
-    <div className="ml-60" class="prose prose-lg">
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, p: 3, position:"center" , width: { sm: `calc(100% - ${drawerWidth}px)` } }}
-      >
-        <Toolbar />
-        <GS/>
-      </Box>
+    <div className="ml-60 prose prose-2xl">
+      <div className="flex ml-24 pr-64">
+        <div className="flex-row ">
+          <Box
+            component="main"
+            sx={{ flexGrow: 1, p: 3, position:"center" }}
+          >
+            <Toolbar />
+            <GS/>
+          </Box>
+        </div>
+      </div>
     </div>
   );
 }
