5b7b1feb3b984519b7dafadfb09cbe04ada059ee
lucas bandeira <lucas4workl8tr@gmail.com>
2023-06-06 14:52:42 -0300

    menu items: bluetoothAdapter, bluetoothState,Boding, Discovery and types added

--- docs/src/components/MenuItems/MenuItems2.jsx
+++ docs/src/components/MenuItems/MenuItems2.jsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
-import ListSubheader from '@mui/material/ListSubheader';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Collapse from '@mui/material/Collapse';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import StarBorder from '@mui/icons-material/StarBorder';
 import SubjectIcon from '@mui/icons-material/Subject';
-import ArticleIcon from '@mui/icons-material/Article';
 import Link from "next/link";
 import {useRouter} from "next/router";
 
@@ -58,6 +54,70 @@ export default function Capabilities() {
         </List>
 
         <List component="div1" disablePadding>
+          <ListItemButton className={router.pathname === "/bluetoothState" ? "bg-[#b33ce6]" : ""}>
+            <ListItemIcon>
+            </ListItemIcon>
+            <Link href='/bluetoothState'>
+              <ListItemText primary="Bluetooth State"
+                            primaryTypographyProps={{
+                              fontSize: 12,
+                              fontWeight: 'bold',
+                              letterSpacing: 0,}}
+              />
+            </Link>
+
+          </ListItemButton>
+        </List>
+
+        <List component="div1" disablePadding>
+          <ListItemButton className={router.pathname === "/bonding" ? "bg-[#b33ce6]" : ""}>
+            <ListItemIcon>
+            </ListItemIcon>
+            <Link href='/bonding'>
+              <ListItemText primary="Bonding Button"
+                            primaryTypographyProps={{
+                              fontSize: 12,
+                              fontWeight: 'bold',
+                              letterSpacing: 0,}}
+              />
+            </Link>
+
+          </ListItemButton>
+        </List>
+
+        <List component="div1" disablePadding>
+          <ListItemButton className={router.pathname === "/discovery" ? "bg-[#b33ce6]" : ""}>
+            <ListItemIcon>
+            </ListItemIcon>
+            <Link href='/discovery'>
+              <ListItemText primary="Discovery"
+                            primaryTypographyProps={{
+                              fontSize: 12,
+                              fontWeight: 'bold',
+                              letterSpacing: 0,}}
+              />
+            </Link>
+
+          </ListItemButton>
+        </List>
+
+        <List component="div1" disablePadding>
+          <ListItemButton className={router.pathname === "/types" ? "bg-[#b33ce6]" : ""}>
+            <ListItemIcon>
+            </ListItemIcon>
+            <Link href='/types'>
+              <ListItemText primary="Types"
+                            primaryTypographyProps={{
+                              fontSize: 12,
+                              fontWeight: 'bold',
+                              letterSpacing: 0,}}
+              />
+            </Link>
+
+          </ListItemButton>
+        </List>
+
+        <List component="div1" disablePadding>
           <ListItemButton className={router.pathname === "/gatt" ? "bg-[#b33ce6]" : ""}>
             <ListItemIcon>
             </ListItemIcon>
@@ -75,8 +135,5 @@ export default function Capabilities() {
 
       </Collapse>
     </List>
-
-  
-    
   );
 }
