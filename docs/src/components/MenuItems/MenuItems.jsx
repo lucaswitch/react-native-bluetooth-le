652b440969c8f8ae9b7d24ef12dca86d8a144f8a
lucas bandeira <lucas4workl8tr@gmail.com>
2023-06-10 20:09:07 -0300

    [Style] - commented headbar

--- docs/src/components/MenuItems/MenuItems.jsx
+++ docs/src/components/MenuItems/MenuItems.jsx
@@ -104,42 +104,42 @@ export default function GettingStarted(props) {
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          backgroundColor:'#b33ce6',
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
-      >
-        <Toolbar>
-        <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-          </Typography>
-          {/*<Search>*/}
-          {/*  <SearchIconWrapper>*/}
-          {/*    <SearchIcon />*/}
-          {/*  </SearchIconWrapper>*/}
-          {/*  <StyledInputBase*/}
-          {/*    placeholder="Search…"*/}
-          {/*    inputProps={{ 'aria-label': 'search' }}*/}
-          {/*  />*/}
-          {/*</Search>*/}
-        </Toolbar>
-      </AppBar>
+      {/*<AppBar*/}
+      {/*  position="fixed"*/}
+      {/*  sx={{*/}
+      {/*    backgroundColor:'#b33ce6',*/}
+      {/*    width: { sm: `calc(100% - ${drawerWidth}px)` },*/}
+      {/*    ml: { sm: `${drawerWidth}px` },*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  /!*<Toolbar>*!/*/}
+      {/*  /!*<IconButton*!/*/}
+      {/*  /!*    color="inherit"*!/*/}
+      {/*  /!*    aria-label="open drawer"*!/*/}
+      {/*  /!*    edge="start"*!/*/}
+      {/*  /!*    onClick={handleDrawerToggle}*!/*/}
+      {/*  /!*    sx={{ mr: 2, display: { sm: 'none' } }}*!/*/}
+      {/*  /!*  >*!/*/}
+      {/*  /!*    <MenuIcon />*!/*/}
+      {/*  /!*  </IconButton>*!/*/}
+      {/*  /!*  <Typography*!/*/}
+      {/*  /!*    variant="h6"*!/*/}
+      {/*  /!*    noWrap*!/*/}
+      {/*  /!*    component="div"*!/*/}
+      {/*  /!*    sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}*!/*/}
+      {/*  /!*  >*!/*/}
+      {/*  /!*  </Typography>*!/*/}
+      {/*  /!*  /!*<Search>*!/*!/*/}
+      {/*  /!*  /!*  <SearchIconWrapper>*!/*!/*/}
+      {/*  /!*  /!*    <SearchIcon />*!/*!/*/}
+      {/*  /!*  /!*  </SearchIconWrapper>*!/*!/*/}
+      {/*  /!*  /!*  <StyledInputBase*!/*!/*/}
+      {/*  /!*  /!*    placeholder="Search…"*!/*!/*/}
+      {/*  /!*  /!*    inputProps={{ 'aria-label': 'search' }}*!/*!/*/}
+      {/*  /!*  /!*  />*!/*!/*/}
+      {/*  /!*  /!*</Search>*!/*!/*/}
+      {/*  /!*</Toolbar>*!/*/}
+      {/*</AppBar>*/}
       <Box
         component="nav"
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
