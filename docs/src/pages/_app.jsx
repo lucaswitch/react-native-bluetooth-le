4e1e35b065eb2f53d52b93511e63eb030a913357
lucas bandeira <lucas4workl8tr@gmail.com>
2023-06-10 22:07:32 -0300

    <Refactor>: blank line and unused imports removed

--- docs/src/pages/_app.jsx
+++ docs/src/pages/_app.jsx
@@ -3,16 +3,12 @@ import "prismjs/themes/prism-tomorrow.css";
 import { MDXProvider } from "@mdx-js/react";
 import '../styles/global.scss';
 import NavBarItems from "@/components/NavBarItems/NavBarItems";
-import SearchAppBar from "@/components/AppBar/AppBar";
-import StickyFooter from "@/components/StickyFooter/StickyFooter";
-
-
 
 export default function App({ Component, pageProps }) {
   return (
     <MDXProvider>
       <NavBarItems/>
       <Component {...pageProps} />
-      </MDXProvider>
+    </MDXProvider>
   );
 }
