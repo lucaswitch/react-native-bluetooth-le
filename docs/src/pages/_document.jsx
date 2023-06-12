03a6299b34705371886db0205982c3a47a284774
lucas bandeira <lucas4workl8tr@gmail.com>
2023-06-12 17:23:54 -0300

    <feat>: added dracula theme to highlight code with mdx

--- docs/src/pages/_document.jsx
+++ docs/src/pages/_document.jsx
@@ -9,6 +9,8 @@ export default class MyDocument extends Document{
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
           <link href="https://cdn.jsdelivr.net/npm/daisyui@2.13.6/dist/full.css" rel="stylesheet" type="text/css" />
+          <link rel="stylesheet" href="https://unpkg.com/dracula-prism/dist/css/dracula-prism.css"
+          ></link>
           <link rel="shortcut icon" href="https://flowbite.com/docs/images/logo.svg" />
         </Head>
         <body>
